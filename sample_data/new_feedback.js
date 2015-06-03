aeac7024e9ee3993dda3dcac827ffd8908c04161
sidak <spssachdeva21695@gmail.com>
2015-06-03 18:25:21 -0400

    data of recent incoming feedback & update .gitignore

---
+++ sample_data/new_feedback.js
@@ -0,0 +1,53 @@
+var new_data = [
+				{
+					"name":"a",
+					"consumer_ratings":[2,3,4,1,4,5,2],
+					"consumer_relevance":[2,4,5,1,2,4,3],
+					"agg_rating_score":-1,
+					
+				},
+				{
+					"name":"b1",
+					"agg_rating_score":-1,
+					"consumer_ratings":[],
+					"consumer_relevance":[],
+					
+				},
+				{	
+					"name":"b2",
+					"agg_rating_score":-1,
+					"consumer_ratings":[1,2,4,2],
+					"consumer_relevance":[4,3,1,2],
+					
+				},	
+				{
+					"name":"c1",
+					"agg_rating_score":-1,
+					"consumer_ratings":[4,4,5,3],
+					"consumer_relevance":[5,3,4,3],
+					
+				},
+				{					
+					"name":"c2",
+					"agg_rating_score":-1,
+					"consumer_ratings":[3,2],
+					"consumer_relevance":[4,1],
+					
+				},
+				{
+					"name":"c3",
+					"agg_rating_score":-1,
+					"consumer_ratings":[4,4,3,2,1,2,4,2,3],
+					"consumer_relevance":[4,5,1,5,3,2,1,3,2],
+					
+				},
+				{
+					"name":"c4",
+					"agg_rating_score":-1,
+					"consumer_ratings":[1,1,1,1,5,5,4,3,2,1,4],
+					"consumer_relevance":[5,5,5,5,1,1,1,1,4,5,4],
+					
+				}
+					
+			];
+module.exports=new_data;
