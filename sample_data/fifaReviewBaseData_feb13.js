eb65422d63790a3966b83900fec3e6cbd239fe81
sidak <spssachdeva21695@gmail.com>
2016-02-27 04:19:18 +0530

    Add office data file and fifa base file

---
+++ sample_data/fifaReviewBaseData_feb13.js
@@ -0,0 +1,69 @@
+var data=[
+	{
+		"name":"_meta",
+		"root":"fifa"
+	},
+	{
+		"name":"fifa",
+		"agg_rating_score":0,
+		"own_rating_cont":0,
+		"children_rating_cont":0,
+		"own_wmean_rating":0,						
+		"universe_wmean_rating":0,
+		"consumer_ratings":[],
+		"consumer_relevance":[],
+		"review_dates":[],
+		"consumer_feedback_count":0,
+		"rating_trust_value":0,
+		"trust_votes":0,
+		"children":[{"name":"fifa14","wt":0.495123},{"name":"fifa15","wt":0.504877}],
+		"parent":[]
+	},
+	{
+		"name":"fifa14",
+		"agg_rating_score":0,
+		"own_rating_cont":0,
+		"children_rating_cont":0,
+		"own_wmean_rating":0,						
+		"universe_wmean_rating":0,
+		"consumer_ratings":[],
+		"consumer_relevance":[],
+		"review_dates":[],
+		"consumer_feedback_count":0,
+		"rating_trust_value":0,
+		"trust_votes":0,
+		
+		"children":[
+				{"name":"FIFA-14-PlayStation-4","wt":0.355106 },
+				{"name":"FIFA-14-Playstation-3","wt":0.280589 },
+				{"name":"FIFA-14-Xbox-360","wt":0.228611 },
+				{"name":"FIFA-14-Xbox-One","wt":0.135695 }
+			],
+		"parent":["fifa"]
+	},
+	{
+		"name":"fifa15",
+		"agg_rating_score":0,
+		"own_rating_cont":0,
+		"children_rating_cont":0,
+		"own_wmean_rating":0,						
+		"universe_wmean_rating":0,
+		"consumer_ratings":[],
+		"consumer_relevance":[],
+		"review_dates":[],
+		"consumer_feedback_count":0,
+		"rating_trust_value":0,
+		"trust_votes":0,
+		
+		"children":[
+				{"name":"FIFA-15-PlayStation-4","wt":0.301595 },
+				{"name":"FIFA-15-PlayStation-3","wt":0.341932 },
+				{"name":"FIFA-15-Xbox-360","wt":0.241557},
+				{"name":"FIFA-15-Xbox-One","wt":0.114916}
+			],
+		"parent":["fifa"]
+	},
+	
+];
+
+module.exports=data;
